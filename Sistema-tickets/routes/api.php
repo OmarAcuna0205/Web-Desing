d45a232dd84fae5967365a3bbc23567981b7bba3
Omar Acuña <13097@ulsachihuahua.edu.mx>
2026-03-18 15:56:38 -0600

    added blade views for tickets, implemented web controller, updated web routes

--- Sistema-tickets/routes/api.php
+++ Sistema-tickets/routes/api.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TicketController;
 
-// Esta UNICA linea registra las 5 rutas del CRUD automaticamente:
-Route::apiResource('tickets', TicketController::class);
+// Le agregamos "names" para que no choque con las rutas Web
+Route::apiResource('tickets', TicketController::class)->names('api.tickets');
--- Sistema-tickets/routes/web.php
+++ Sistema-tickets/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TicketWebController;
 
+// Si entran a la raíz, los mandamos al listado de tickets
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('tickets.index');
 });
+
+// Registra las 7 rutas web de un trancazo
+Route::resource('tickets', TicketWebController::class);
